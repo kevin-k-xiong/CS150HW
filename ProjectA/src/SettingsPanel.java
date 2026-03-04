788e8602584e450afb847ad19d87afd140c39856
kevin-k-xiong <Xiongkk32@gmail.com>
2026-03-04 10:56:02 -0600

    progress

--- ProjectA/src/SettingsPanel.java
+++ ProjectA/src/SettingsPanel.java
@@ -1,22 +1,29 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+
 import java.awt.*;
 
 public class SettingsPanel extends JPanel {
 
     private JButton reset;
-    private JButton importExport;
-    private JButton export;
+    private JButton confirmReset;
+    private JButton cancelReset;
+    private JDialog resetWindow;
+    private JButton importExportBtn;
+    private JButton exportBtn;
+    private JButton importBtn;
     private JDialog data;
 
     public SettingsPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        // reset = new JButton("Reset Data");
-        export = new JButton("Export Data");
+        importExportBtn = new JButton("Import/Export");
+        reset = new JButton("Reset Data");
 
-        // add(reset);
-        add(export);
+        add(importExportBtn);
+        add(reset);
 
-        export.addActionListener(e -> importOrExport());
+        importExportBtn.addActionListener(e -> importOrExport());
+        reset.addActionListener(e -> resetConfirmation());
 
     }
 
@@ -25,20 +32,47 @@ public class SettingsPanel extends JPanel {
         data = new JDialog(mainFrame, "Import/Export", Dialog.ModalityType.APPLICATION_MODAL);
         data.setLayout(new FlowLayout());
         data.getContentPane();
-        importExportBtn();
+        dataBtn();
         data.pack();
         data.setLocationRelativeTo(this);
         data.setVisible(true);
     }
 
+    public void resetConfirmation() {
+        Window mainFrame = SwingUtilities.getWindowAncestor(this);
+        resetWindow = new JDialog(mainFrame, "Are You Sure?", Dialog.ModalityType.APPLICATION_MODAL);
+        resetWindow.setLayout(new FlowLayout());
+        resetWindow.getContentPane();
+        resetChoices();
+        resetWindow.pack();
+        resetWindow.setLocationRelativeTo(this);
+        resetWindow.setVisible(true);
+    }
+
+    public void resetChoices() {
+        confirmReset = new JButton("Yes");
+        cancelReset = new JButton("No");
+
+        JPanel resetPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        resetPanel.add(confirmReset);
+        resetPanel.add(cancelReset);
+
+        resetWindow.add(resetPanel, BorderLayout.SOUTH);
+
+    }
+
     // Create the import/export
-    public void importExportBtn() {
-        reset = new JButton("Reset Data");
+    public void dataBtn() {
+        importBtn = new JButton("Import Data");
+        exportBtn = new JButton("Export Data");
 
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        bottomPanel.add(reset);
+        JPanel dataPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
-        data.add(bottomPanel, BorderLayout.SOUTH);
+        dataPanel.add(importBtn);
+        dataPanel.add(exportBtn);
+
+        data.add(dataPanel, BorderLayout.SOUTH);
     }
 
 }
